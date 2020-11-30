36d0b709c364d23ddd610b07eb1fc22849326708
Max <maximilienlarue@gmail.com>
2020-11-30 12:30:27 +0100

    hack things to work on itchio

--- src/Preferences.ts
+++ src/Preferences.ts
@@ -7,12 +7,20 @@ export enum Keyboard {
 
 export const KEYBOARD_COOKIE = "KEYBOARD_COOKIE"
 
+let keyboardGlobal: Keyboard | null = null
+
 export default class Preferences {
   get keyboard(): Keyboard {
-    return Cookies.get(KEYBOARD_COOKIE) === Keyboard.AZERTY ? Keyboard.AZERTY : Keyboard.QWERTY
+    if (!keyboardGlobal) {
+      const fromCookies = Cookies.get(KEYBOARD_COOKIE) === Keyboard.AZERTY ? Keyboard.AZERTY : Keyboard.QWERTY
+      keyboardGlobal = fromCookies
+      return fromCookies
+    }
+    return keyboardGlobal
   }
   set keyboard(to: Keyboard) {
     Cookies.set(KEYBOARD_COOKIE, to)
+    keyboardGlobal = to
   }
   public getOtherKeyboard(from: Keyboard): Keyboard {
     return this.keyboard === Keyboard.AZERTY ? Keyboard.QWERTY : Keyboard.AZERTY
